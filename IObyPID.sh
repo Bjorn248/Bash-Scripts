f00d02cf5947568bf9391fc9686b9f34124c1435
Bjorn <bjorn248@gmail.com>
2012-09-21 00:46:08 +0000

    fixed indent

--- IObyPID.sh
+++ IObyPID.sh
@@ -13,17 +13,17 @@ if [ -z "$1" ];
 fi
 
 while [ true ]
-    do
-        R=$(echo `cat /proc/$PID/io` | cut -d' ' -f10)
-        R=$(($R/1024));
-        W=$(echo `cat /proc/$PID/io` | cut -d' ' -f12)
-        W=$(($W/1024));
-        sleep 1;
-        R2=$(echo `cat /proc/$PID/io` | cut -d' ' -f10)
-        R2=$(($R2/1024));
-        W2=$(echo `cat /proc/$PID/io` | cut -d' ' -f12)
-        W2=$(($W2/1024));
-        rKB=$(($R2 - $R));
-        wKB=$(($W2 - $W));
-        echo "$rKB KB/s read $wKB KB/s write"
+do
+    R=$(echo `cat /proc/$PID/io` | cut -d' ' -f10)
+    R=$(($R/1024));
+    W=$(echo `cat /proc/$PID/io` | cut -d' ' -f12)
+    W=$(($W/1024));
+    sleep 1;
+    R2=$(echo `cat /proc/$PID/io` | cut -d' ' -f10)
+    R2=$(($R2/1024));
+    W2=$(echo `cat /proc/$PID/io` | cut -d' ' -f12)
+    W2=$(($W2/1024));
+    rKB=$(($R2 - $R));
+    wKB=$(($W2 - $W));
+    echo "$rKB KB/s read $wKB KB/s write"
 done
